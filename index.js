3bd5f673e855c659e8fcc6fcff81f1740561057c
Kayzer <max.mlv2210@gmail.com>
2023-12-25 19:22:25 +0100

    début de correction

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+// Ordre de Priorité en CSS : !important > ID > CLasse > BaliseHTML
 
-// Ranger la sidebar si on click sur le contenu principal
+const sidebar = document.getElementById("side-bar");
 
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
-
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
-
-document.addEventListener("DOMContentLoaded", () => {
-  let sidebar = document.getElementById("side-bar");
-  let toggleBtn = document.getElementById("btn");
-  let body = document.querySelector("body");
-
-  toggleBtn.addEventListener("click", () => {
-    let currentLeft = parseInt(window.getComputedStyle(sidebar).left, 10);
-    let newLeft = currentLeft === 0 ? -230 : 0;
-    sidebar.style.left = newLeft + "px";
-
-    toggleBtn.classList.toggle("cross", newLeft === 0);
-  });
-
-  body.addEventListener("click", () => {
-    if (parseInt(window.getComputedStyle(sidebar).left, 10) === 0) {
-      sidebar.style.left = "-230px";
-      toggleBtn.classList.remove("cross");
-    }
-  });
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
 });
