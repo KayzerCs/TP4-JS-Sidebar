3e3a5d3174c564cc0b37f5dd0081824b1d5eea8d
Kayzer <max.mlv2210@gmail.com>
2023-12-26 15:47:39 +0100

    Correction terminé

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Ordre de Priorité en CSS : !important > ID > CLasse > BaliseHTML
 
 const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
 
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
 });
